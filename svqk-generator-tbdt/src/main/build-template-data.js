16a6f959268794ef8abdc65c68bea289e325dc67
motoki.himeno <motoki.himeno@monocrea.co.jp>
2026-01-07 16:05:14 +0900

    feat: add svqk-generator-tbdt

--- svqk-generator-tbdt/src/main/build-template-data.js
+++ svqk-generator-tbdt/src/main/build-template-data.js
@@ -1,36 +1,56 @@
 const English = Java.type('org.atteo.evo.inflector.English');
 
-function build_name_variation(pascalNm) {
+const buildPackageName = (domainPkgNm) => ({
+  domain: domainPkgNm,
+  interfaces: domainPkgNm.replace('.domain.', '.interfaces.'),
+});
+
+const buildNameVariation = (inputNm) => {
+  const pascalNm = inputNm.charAt(0).toUpperCase() + inputNm.slice(1);
+
   return {
     pascal: pascalNm,
     camel: pascalNm.charAt(0).toLowerCase() + pascalNm.slice(1),
     allCaps: pascalNm.toUpperCase(),
     plural: English.plural(pascalNm.toLowerCase()),
-    kebab: pascalNm
-      .replace(/([a-z])([A-Z])/g, '$1-$2')
-      .replace(/[A-Z]/g, (letter) => letter.toLowerCase()),
+    kebab: pascalNm.replaceAll(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(),
     firstLetter: pascalNm.charAt(0).toLowerCase(),
   };
-}
+};
 
-function build_template_data(metadata) {
-  // TODO field name process
+const buildTemplateData = (metadata, metadataList) => {
+  const idField = metadata.fields.find((field) => field.id) ?? {};
+
   return {
-    domainPkgNm: metadata.packageName,
-    interfacesPkgNm: metadata.packageName.replace('.domain.', '.interfaces.'),
-    entityNm: build_name_variation(metadata.className.replace('Entity', '')),
+    pkgNm: buildPackageName(metadata.packageName),
+    entityNm: buildNameVariation(metadata.className.replace('Entity', '')),
     fields: metadata.fields,
-    // TODO add prop
+    idField: idField,
+    nonIdFields: metadata.fields.filter((field) => !field.id),
+    compIdFields: metadataList.find(
+      (meta) => meta.className === idField.javaType,
+    )?.fields,
   };
+};
+
+const processFieldName = (metadataList) => {
+  metadataList.forEach((meta) => {
+    meta.fields.forEach((field) => {
+      field.fieldNm = buildNameVariation(field.fieldName);
+    });
+  });
+};
+
+function build() {
+  const metadataList = JSON.parse(project.getProperty('metadata'));
+
+  processFieldName(metadataList);
+
+  const templateData = metadataList.map((meta) =>
+    buildTemplateData(meta, metadataList),
+  );
+
+  project.setProperty('template.data', JSON.stringify(templateData, null, 2));
 }
 
-const metadataList = JSON.parse(project.getProperty('metadata'));
-
-const templateData = metadataList.map((metadata) => {
-  build_template_data(metadata);
-});
-
-// TODO 書き出しはantにパスかもしれない
-const writer = new java.io.FileWriter(new java.io.File('input.json'));
-writer.write(JSON.stringify(templateData, null, 2));
-writer.close();
+build();
